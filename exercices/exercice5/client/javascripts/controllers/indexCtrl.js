19a4dac3f1b4090c888dd495a6a1f99be222cf04
thibaut-cllrd <thibautcolliard2005@gmail.com>
2024-02-02 11:03:52 +0100

    Exercice 5 finit

--- exercices/exercice5/client/javascripts/controllers/indexCtrl.js
+++ exercices/exercice5/client/javascripts/controllers/indexCtrl.js
@@ -34,6 +34,13 @@ function chargerPlayerSuccess(data, text, jqXHR) {
     var cmbJoueurs = document.getElementById("cmbJoueurs");
 	// A COMPLETER!!! selon la logique suivante:
 	// cmbJoueurs.options[cmbJoueurs.options.length] = new Option(<ce qui sera affiché>, <la valeur de la cellule>));
+    $(data).find("joueur").each(function() {
+        var joueur = new Joueur();
+        joueur.setNom($(this).find("nom").text());
+        joueur.setPoints($(this).find("points").text());
+        cmbJoueurs.options[cmbJoueurs.options.length] = new Option(joueur, JSON.stringify(joueur));
+    });
+
 }
 
 /**
